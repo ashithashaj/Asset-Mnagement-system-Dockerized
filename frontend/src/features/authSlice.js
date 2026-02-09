45ffe982e7e94a785e88032774c1b3a7c8e44d90
Ashithashajip <120626543+Ashithashajip@users.noreply.github.com>
2026-02-09 17:24:48 +0530

    Update API_URL to Render backend

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = "https://asset-mnagement-system-dockerized.onrender.com/api";
 
 /* ================= LOGIN ================= */
 export const loginUser = createAsyncThunk(
